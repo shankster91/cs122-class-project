183c628dd06b63e232b96ab0f0ce309cf6277c3c
Carly Schippits <cschippits@cs.uchicago.edu>
2021-02-25 11:11:20 -0600

    more progress on pres data

--- data/pres_data_county_to_zip.py
+++ data/pres_data_county_to_zip.py
@@ -16,7 +16,7 @@ import pandas as pd
 
 def county_to_zip():
     '''
-    Converts the 2016 presidential election results data from county level to
+    Converts the presidential election results data from county level to
     zip code level, and writes the data to a csv called pres_data.csv.
 
     Inputs:
@@ -25,29 +25,24 @@ def county_to_zip():
     Outputs:
         None
     '''
-    pres_data = pd.read_csv('raw_data/countypres_2000-2016.csv')
+    pres_data = pd.read_csv('raw_data/2016_US_County_Level_Presidential_Results.csv')
     county_to_zip = pd.read_excel('raw_data/COUNTY_ZIP_122016.xlsx')
     county_to_zip.columns = [col.lower() for col in county_to_zip.columns]
-    pres_data = pres_data.loc[pres_data['year']==2016]
-    pres_data = pres_data.drop(['county', 'state'], axis=1)
-    pres_data.rename(columns = {'FIPS' : 'county', 'state_po' : 'state'}, inplace=True)
-    pres_data['party'].fillna('other', inplace=True)
+    pres_data.rename(columns = {'combined_fips' : 'county', 'state_abbr' : 'state'}, inplace=True)
+    pres_data['votes_other'] = pres_data['total_votes'] - pres_data['votes_dem'] - pres_data['votes_gop']
     pres_data = pres_data.merge(county_to_zip, how='left', on='county')
-    cols_to_keep = ['state', 'zip', 'party', 'candidatevotes', 'totalvotes', 'res_ratio']
+    cols_to_keep = ['state', 'zip', 'votes_dem', 'votes_gop', 'votes_other', 'total_votes', 'res_ratio']
     pres_data = pres_data[cols_to_keep]
-    vote_counts = ['candidatevotes', 'totalvotes']
-    pres_data[vote_counts] = pres_data[vote_counts].multiply(pres_data['res_ratio'], axis=0)
-    # fix ak data, count zips, see if accurate
-    pres_data = pres_data.groupby(['state', 'zip', 'party'])[vote_counts].sum()
+    party_votes = ['votes_dem', 'votes_gop', 'votes_other']
+    pres_data[votes] = pres_data[party_votes + ['total_votes']].multiply(pres_data['res_ratio'], axis=0)
+    # fix ak data
+    pres_data = pres_data.groupby(['state', 'zip'])[party_votes].sum()
+    # fix ak data
     # count 33,000
     # check total vote for accuracy          
-    pres_data['vote_perc'] = (data['candidatevotes'] / data['totalvotes']) * 100
-    # check that zips in ame county have same perc
-    cols_to_drop = ['candidatevotes', 'totalvotes']
-    pres_data = pres_data.drop(cols_to_drop, axis=1)
-    pres_data =  pres_data.pivot_table(index=["state", "zip"],
-                                       columns='party',
-                                       values='vote_perc') # long to wide
+    pres_data[votes] = pres_data[party_votes].divide(pres_data['total_votes']) * 100
+    # check that zips in same county have same perc
+    pres_data = pres_data.drop(['total_votes'], axis=1)
     # drop absentee, no state_po, nan zip etc.
     data.to_csv('pres_data.csv', index=False)
     # count 33,000
