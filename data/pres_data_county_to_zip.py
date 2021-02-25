1d27c444c94b29be3a7c25f109a5b0bf7c9be3f0
Carly Schippits <cschippits@cs.uchicago.edu>
2021-02-24 20:46:30 -0600

    inital progress cleaning the presidential data

--- data/pres_data_county_to_zip.py
+++ data/pres_data_county_to_zip.py
@@ -1,36 +1,49 @@
 '''
 This file creates the final dataset for 2016 presidential election results by
 converting the data from county level to zip code level.
+
+For each zip code, the county-level results for the county(ies) containing
+the zip code are multiplied by the fraction of the county's population that
+resides in that zip code. When a zip code straddels multiple counties, we
+take the sum of the population-weighted results across the counties that the
+zip code straddles.
+
+To download the xlrd module: pip install xlrd
+To download the openpyxl module: pip install openpyxl
 '''
 
 import pandas as pd
 
 def county_to_zip():
     '''
-    Converts the presidential election results data from county level to zip
-    code level. For a given zip code, the county-level results for the county containing that zip code are multiplied by the fraction of the county's population that lives in that zip code. When a zipcode straddles multiple counties, the same process is followed but the population-weighted results for each county are summed across the counties in which the zip code lies.
+    Converts the 2016 presidential election results data from county level to
+    zip code level, and writes the data to a csv called pres_data.csv.
 
     Inputs:
         None
     
-    Output:
-        
+    Outputs:
+        None
     '''
-    eviction_data = pd.read_csv('eviction_data_tract.csv')
-    tract_to_zip = pd.read_csv('tract_zip.csv')
-    eviction_data = eviction_data.loc[eviction_data['filing_year']==2019]
-    eviction_data = eviction_data.merge(tract_to_zip, how='left', on='tract')
-    cols_to_keep = ['zip', 'eviction_filings_total', 'RES_RATIO']
-    eviction_data = eviction_data[cols_to_keep]
-    eviction_data = eviction_data.groupby('zip').apply(lambda row:
-                          sum(row['eviction_filings_total'] * row['RES_RATIO']))
-    eviction_data.name = 'eviction_filings'
-    eviction_data.index = eviction_data.index.astype(str)
-                          
-    data = data.merge(eviction_data, how='left', left_index=True, right_index=True)
-    data['eviction_filings_rate'] = (data['eviction_filings'] / data['rental_units']) * 100
-    cols_to_drop = ['eviction_filings', 'rental_units']
-    data = data.drop(cols_to_drop, axis=1)
-    return data
+    pres_data = pd.read_csv('raw_data/countypres_2000-2016.csv')
+    county_to_zip = pd.read_excel('raw_data/COUNTY_ZIP_122016.xlsx')
+    county_to_zip.columns = [col.lower() for col in county_to_zip.columns]
+    pres_data = pres_data.loc[pres_data['year']==2016]
+    pres_data = pres_data.drop(['county'], axis=1)
+    pres_data.rename(columns = {'FIPS' : 'county'}, inplace=True)
 
-
+    pres_data = pres_data.merge(county_to_zip, how='left', on='county') # left?, county data type (leading zeros)
+    cols_to_keep = ['state_po', 'zip', 'party', 'candidatevotes', 'RES_RATIO']
+    pres_data = pres_data[cols_to_keep]
+    pres_data = pres_data.groupby('zip').apply(lambda row:
+                                  sum(row['candidatevotes'] * row['RES_RATIO']))             
+    pres_data['vote_perc'] = (data['candidatevotes'] / data['totalvotes']) * 100
+    cols_to_drop = ['candidatevotes', 'totalvotes']
+    pres_data = pres_data.drop(cols_to_drop, axis=1)
+    pres_data =  pres_data.pivot_table(index=["state_po", "zip"],
+                                       columns='party',
+                                       values='vote_perc') # long to wide
+    # drop absentee, no state_po, etc.
+    data.to_csv('pres_data.csv', index=False)
+    # count 3,000
+    # count 33,000
