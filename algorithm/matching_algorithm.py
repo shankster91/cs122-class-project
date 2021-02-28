0f81c694f49ee3db9b1c6e02ba9b2419b6038b6c
Carly Schippits <cschippits@cs.uchicago.edu>
2021-02-27 19:23:01 -0600

    more progress on the algorithm

--- algorithm/matching_algorithm.py
+++ algorithm/matching_algorithm.py
@@ -6,19 +6,63 @@ codes in that state that are most similar to the user-specified zip code
 (i.e. it selects the five zip codes with the smallest sum of squared differences).
 '''
 
+import re
 import math
 import sqlite3
 import pandas as pd
 
 
-def create_sql_query(state, zip_code):
+class zipInfo(object):
     '''
-    Create the SQL query given a user-specified state and a user-specified zip
-    code.
+    Class for representing zip-code-level data and for keeing track of the zip
+      codes that are most siliar to the user-specified zip code.
+    '''
+
+    def __init__(self, args_from_ui):
+        '''
+        Construct a data structure to hold the zip-code-level data
+        and to keep track of the most similar zip codes.
+
+        Inputs:
+            args_from_ui: a dictionary containing the user-specified inputs.
+        '''
+        state = args_from_ui['state']
+        start_zip = args_from_ui['zip']
+        data = pull_data(state, start_zip)
+        self.columns = data.columns
+        self.start_zip_data = data[data['zip'] == start_zip]
+        self.data = data[data['zip'] != start_zip]
+        self.best_zips = [(None, math.inf)] * 5
+
+
+def pull_data(state, start_zip):
+    '''
+    Pull zip-code-level data for the starting zip code and for all zip codes in
+      the user-specified state.
 
     Inputs:
-        state: a string representing the postal abbreviation for a U.S. state
-        zip_code: an integer representing a U.S. zip code
+        state: a string representing the postal abbreviation for a U.S. state.
+        start_zip: an integer representing a U.S. zip code.
+
+    Outputs:
+        A pandas dataframe.
+    '''
+    conn = sqlite3.connect('zip_db.sqlite3')
+    sql_query, args = create_sql_query(state, start_zip)
+    data = pd.read_sql_query(sql_query, conn, params=args)
+    conn.close()
+
+    data.drop('state', axis=1, inplace=True)
+    return data.apply(pd.to_numeric)
+
+
+def create_sql_query(state, start_zip):
+    '''
+    Create the SQL query given a state and a starting zip code.
+
+    Inputs:
+        state: a string representing the postal abbreviation for a U.S. state.
+        start_zip: an integer representing a U.S. zip code.
     
     Outputs:
         A tuple containing 1) a string representing a SQL query, and 2) a tuple
@@ -35,8 +79,28 @@ def create_sql_query(state, zip_code):
                   JOIN zillow USING (zip)
                   WHERE c.state = ?
                   OR c.zip = ?;'''
-    arg_lst = (state, zip_code)
+    arg_lst = (state, start_zip)
     return (sql_query, arg_lst)
+
+
+def get_table_counts(columns): # maybe this should be done only once (i.e. not every time the user searches)
+    '''
+    Count the number of variables from each table.
+
+    Inputs:
+        columns: a pandas index object containing strings that represent the
+          column/variable names of the data.
+    
+    Outputs:
+        table_counts: a dictionary containing the variable counts for each table.
+    '''
+    table_counts = {'census' : 0, 'biz' : 0, 'school' : 0, 'votes' : 0,
+                    'libraries' : 0, 'museums' : 0, 'walk' : 0, 'property' : 0}
+    for col in columns:
+        for name in table_counts.keys():
+            if col.startswith(name):
+                table_counts[name] += 1
+    return table_counts
 
 
 def find_best_zips(args_from_ui):
@@ -56,21 +120,15 @@ def find_best_zips(args_from_ui):
         return [(None, math.inf)] * 5
     # add assert statement like in PA3?
 
-    state = args_from_ui['state']
-    zip_code = args_from_ui['zip']
+    zip_info = zipInfo(args_from_ui)
 
-    conn = sqlite3.connect('../zip_db.sqlite3')
-    sql_query, args = create_sql_query(state, zip_code)
-    data = pd.read_sql_query(sql_query, conn, params=args)
-    conn.close()
-
-    data.drop('state', axis=1, inplace=True)
-    data = data.apply(pd.to_numeric)
-    start_zip_data = data[data['zip'] == zip_code] # put this in the class?
-    zip_data = data[data['zip'] != zip_code] # put this in the class?
-
+    table_counts = get_table_counts(zip_info.columns)
+    #for zip_code, row in zip_info.data.iterrows():
+    #    zip_info.compute_sq_diff(row)
     # find best zips, normalize the scale of all variables, weights on diff tables, average across dist, average across table, deal with nas
-    return data # edit return value
+
+    return zip_info.data
+    #return start_zip_info.best_zips
 
 
 
@@ -83,5 +141,81 @@ def find_best_zips(args_from_ui):
 # check size of joined dataset without WHERE statement
 
 # in orig algorithm, scale all variables to normalize
+# comments
 
 # pylint, git, close ssh
+
+
+    def compute_sq_diff(self, zip_data):
+        '''
+        Compute the sum of squared differences between the set of attributes for
+        two zip codes. Update the zip_selector in place with the best zip code
+        matches (i.e. the zip codes that have the least sum of squared
+        differences).
+
+        Inputs:
+            zip_data: a pandas dataframe containing demographic data for a
+              specific zip code.
+
+        Output:
+            None
+        '''
+        _, best5_sq_diff = self.best_zips[4] # sum of squared diffs for the 5th best zip code
+        _, best4_sq_diff = self.best_zips[3] # sum of squared diffs for the 4th best zip code
+        _, best3_sq_diff = self.best_zips[2] # sum of squared diffs for the 3rd best zip code
+        _, best2_sq_diff = self.best_zips[1] # sum of squared diffs for the 2nd best zip code
+        _, best1_sq_diff = self.best_zips[0] # sum of squared diffs for the 1st best zip code
+        sq_diff = 0
+        start_zip_data = self.data.squeeze()
+        zip_data = zip_data.squeeze()
+        for col in zip_data.index:
+            if re.search('age_', col):
+                n = 13 # maybe instead we could do a findall on var_lst, and take the length
+            elif re.search('income', col):
+                n = 10
+            elif re.search('educ', col):
+                n = 7
+            elif re.search('occupations', col):
+                n = 5
+            elif re.search('race', col):
+                n = 6
+            elif re.search('ethnicity', col):
+                n = 2
+            elif re.search('born', col):
+                n = 2
+            elif re.search('language', col):
+                n = 2
+            elif re.search('males', col):
+                n = 10
+            elif re.search('housing_', col):
+                n = 2
+            elif re.search('housing', col):
+                n = 3
+            elif re.search('health', col):
+                n = 3
+            elif re.search('struct', col):
+                n = 6
+            elif re.search('male', col):
+                n = 2
+            elif re.search('HHer', col):
+                n = 6
+            else:
+                n = 1
+            if re.search('size', col):
+                sq_diff += ((zip_data[col] - start_zip_data[col]) * 5.5310) ** 2 / (22 * n) # normalize to put the difference in HH size on a scale of 0 to 100 (5.5310 = 100 / (highest mean HH size - lowest mean HH size))
+            else:
+                sq_diff += (zip_data[col] - start_zip_data[col]) ** 2 / (22 * n)
+            if sq_diff >= best5_sq_diff:
+                return None
+        if sq_diff >= best4_sq_diff:
+            index = 4
+        elif sq_diff >= best3_sq_diff:
+            index = 3
+        elif sq_diff >= best2_sq_diff:
+            index = 2
+        elif sq_diff >= best1_sq_diff:
+            index = 1
+        else:
+            index = 0
+        self.best_zips.pop()
+        self.best_zips.insert(index, (zip_data.name, sq_diff))
