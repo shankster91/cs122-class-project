e273dc19511314b79eea0f28097100424e886f52
Caroline Kinnen <ckinnen@cs.uchicago.edu>
2021-03-16 13:52:08 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,20 @@
 #!/bin/bash 
 
-PYTHON="python3"
+# 1. First check to see if the correct version of Python is installed on the local machine 
+echo "1. Checking Python version..."
+REQ_PYTHON_V="370"
+
+ACTUAL_PYTHON_V=$(python -c 'import sys; version=sys.version_info[:3]; print("{0}{1}{2}".format(*version))')
+ACTUAL_PYTHON3_V=$(python3 -c 'import sys; version=sys.version_info[:3]; print("{0}{1}{2}".format(*version))')
+
+if [[ $ACTUAL_PYTHON_V > $REQ_PYTHON_V ]] || [[ $ACTUAL_PYTHON_V == $REQ_PYTHON_V ]];  then 
+    PYTHON="python"
+elif [[ $ACTUAL_PYTHON3_V > $REQ_PYTHON_V ]] || [[ $ACTUAL_PYTHON3_V == $REQ_PYTHON_V ]]; then 
+    PYTHON="python3"
+else
+    echo -e "\tPython 3.7 is not installed on this machine. Please install Python 3.7 before continuing."
+    exit 1
+fi
 
 # 1. What OS are we running on?
 PLATFORM=$($python3 -c 'import platform; print(platform.system())')
