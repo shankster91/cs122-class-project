57a0c7e7294ab20f257e45412a8f1e66e5cc798c
Shashank Bharadwaj <shashab@cs.uchicago.edu>
2021-03-16 15:28:19 -0500

    Cleaned up scraping scripts

--- web_scrape_scripts/weather.py
+++ web_scrape_scripts/weather.py
@@ -1,5 +1,7 @@
 '''
-Pull weather info by zip
+This file scrapes weather information by zip code from weatherbase.com.
+We use Selenium as the page we need to hit uses a JS query to populate results.
+We need to wait for this to occur before we can proceed.
 '''
 
 import bs4
@@ -15,6 +17,16 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.firefox.options import Options
 
 def start_driver(headless=True):
+    '''
+    Starts Selenium webdriver
+    
+    Input:
+    headless (bool): True if user wants to suppress browser opening
+                     and have scraping happen in background
+    
+    Output:
+    driver (Selenium webdriver)
+    '''
     
     ff_options = Options()
     if headless:
@@ -26,20 +38,29 @@ def start_driver(headless=True):
     return driver
 
 def get_weather_by_zip(zip_code, driver=None, headless=True):
+    '''
+    Gets weather info for a single zip code
+
+    Input:
+    zip_code (str): A US zip code
+    driver: Selenium webdriver
+    headless (bool): Bool for whether browser window should be suppressed.
+
+    Output:
+    (temp, precip) (tuple): Avg. annual temp, precipitation for given zip code.
+                            -1 represents missing values.
+    '''
 
     if driver == None:
         driver = start_driver(headless)
-    #time.sleep(1)
 
+    # find search box
     searchbox = driver.find_element_by_id("query")
     searchbox.click()
-    #time.sleep(1)
     searchbox.clear()
-    #time.sleep(1)
     searchbox.send_keys(zip_code)
-    #time.sleep(1)
     button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[3]/div[2]/div/table/tbody/tr[2]/td[4]/form/button")))
-    #driver.find_element_by_xpath("/html/body/div[3]/div[2]/div/table/tbody/tr[2]/td[4]/form/button").click()
+    # Add retry logic for when web page hangs
     try:
         button.click()
     except TimeoutException:
@@ -58,7 +79,6 @@ def get_weather_by_zip(zip_code, driver=None, headless=True):
             button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[3]/div[2]/div/table/tbody/tr[2]/td[4]/form/button"))) 
             button.click() 
 
-    #time.sleep(1)
     page_source = driver.page_source
 
     soup = bs4.BeautifulSoup(page_source, "html5lib")
@@ -67,7 +87,6 @@ def get_weather_by_zip(zip_code, driver=None, headless=True):
     try:
         new_url = "https://www.weatherbase.com" + link['href']
         driver.get(new_url)
-        #time.sleep(1)
 
         fin_page_source = driver.page_source
         soup = bs4.BeautifulSoup(fin_page_source, "html5lib")
@@ -79,22 +98,27 @@ def get_weather_by_zip(zip_code, driver=None, headless=True):
             if table.find("div").text == 'Average Temperature':
                 data_table = table.find_next("table")
                 temp = data_table.find("td", attrs = {"class": "data"}).text
-                #print("Average Temperature:", temp)
             elif table.find("div").text == 'Average Precipitation':
                 data_table = table.find_next("table")
                 precip = data_table.find("td", attrs = {"class": "data"}).text
-                #print("Average Precipitation:", precip)
     except TimeoutException:
         temp = -1
         precip = -1
-    except:
-        temp = -1
-        precip = -1
 
-    #print("Done with zip:", zip_code)
     return (temp, precip)
 
 def get_weather_lst(zip_list, headless=True):
+    '''
+    Gets weather info for a list of zip codes
+
+    Input:
+    zip_list (list): List of US zip codes
+    headless (bool): Bool for whether browser window should be suppressed.
+
+    Output:
+    temp_lst (list): List of avg. annual temps for given zip codes
+    precip_lst(list): List of avg. annual precipitation for given zip codes
+    '''
 
     driver = start_driver(headless)
 
@@ -109,13 +133,23 @@ def get_weather_lst(zip_list, headless=True):
     
         if index % 100 == 0:
             time.sleep(1)
-            #print("Done with zip #", index)
 
     driver.close()
     
     return temp_lst, precip_lst
 
 def weather_to_csv(zip_list, filename, headless=True):
+    '''
+    Gets weather info for a list of zip codes and writes to csv
+
+    Input:
+    zip_list (list): List of US zip codes
+    filename (str): Filename for output
+    headless (bool): Bool for whether browser window should be suppressed.
+
+    Output:
+    No return but will write out a CSV file with zips and weather info
+    '''
 
     temp_lst, precip_lst = get_weather_lst(zip_list, headless)
     pd_dict = {'zip': zip_list, 'temperature': temp_lst, 'precipitation': precip_lst}
@@ -125,6 +159,20 @@ def weather_to_csv(zip_list, filename, headless=True):
     df.to_csv(filename, index = False, mode = 'a', header = False)
 
 def batch_runner(zip_list, filename, headless=True):
+    '''
+    Runs code in batches of zips and returns info on progress.
+    This way if there is an error at some point we don't lose
+    everything.
+
+    Input:
+    zip_list (list): List of US zip codes
+    filename (str): Filename for output
+    headless (bool): Bool for whether browser window should be suppressed.
+
+    Output:
+    No return but will write out a CSV file with zips and weather info,
+    in batches of 100 zips
+    '''
 
     index = 0
     while index < len(zip_list):
@@ -132,32 +180,3 @@ def batch_runner(zip_list, filename, headless=True):
         weather_to_csv(batch, filename, headless)
         index += 100
         print("Batch done, index=", index)
-
-
-
-#delay = 10 # seconds
-#try:
-#    myElem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.ID, 'cell320left')))
-#    print("Page is ready!")
-#except TimeoutException:
-#    print("Loading took too much time!")
-
-#html = driver.page_source
-#time.sleep(2)
-#with open("test.txt", 'w') as sys.stdout:
-#    print(html)
-
-# def get_weather_by_zip(zip_code):
-#     url = "https://www.weatherbase.com/search/search.php3?query=30324"
-#     req = util.get_request(url)
-#     if req:
-#         #text = util.read_request(req)
-#         text = req.text
-#     else:
-#         score =  -1
-#         text = None
-#     if text:
-#         soup = bs4.BeautifulSoup(text, "html5lib")
-#         divs = soup.find_all('div', attrs = {'id': 'cell320left'})
-    
-#     return text
