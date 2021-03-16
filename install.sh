17c52357a9c9b65b4ef56095cc136d89db67b9ae
Caroline Kinnen <ckinnen@cs.uchicago.edu>
2021-03-16 14:10:20 -0500

    updates to install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,31 @@
 #!/bin/bash 
 
-# 1. What OS are we running on?
-PLATFORM=$($python3 -c 'import platform; print(platform.system())')
+# 1. First check to see if the correct version of Python is installed on the local machine 
+echo "1. Checking Python version..."
+REQ_PYTHON_V="370"
 
-echo -e "1. Checking OS Platform..."
+ACTUAL_PYTHON_V=$(python -c 'import sys; version=sys.version_info[:3]; print("{0}{1}{2}".format(*version))')
+ACTUAL_PYTHON3_V=$(python3 -c 'import sys; version=sys.version_info[:3]; print("{0}{1}{2}".format(*version))')
+
+if [[ $ACTUAL_PYTHON_V > $REQ_PYTHON_V ]] || [[ $ACTUAL_PYTHON_V == $REQ_PYTHON_V ]];  then 
+    PYTHON="python"
+elif [[ $ACTUAL_PYTHON3_V > $REQ_PYTHON_V ]] || [[ $ACTUAL_PYTHON3_V == $REQ_PYTHON_V ]]; then 
+    PYTHON="python3"
+else
+    echo -e "\tPython 3.7 is not installed on this machine. Please install Python 3.7 before continuing."
+    exit 1
+fi
+
+echo -e "\t--Python 3.7 is installed"
+
+# 2. What OS are we running on?
+PLATFORM=$($PYTHON -c 'import platform; print(platform.system())')
+
+echo -e "2. Checking OS Platform..."
 echo -e "\t--OS=Platform=$PLATFORM"
 
-# 2. Create Virtual environment 
-echo -e "2. Creating new virtual environment..."
+# 3. Create Virtual environment 
+echo -e "3. Creating new virtual environment..."
 
 # Remove the env directory if it exists 
 if [[ -d env ]]; then 
@@ -15,16 +33,16 @@ if [[ -d env ]]; then
     rm -rf env  
 fi
 
-$python3 -m venv env 
+$PYTHON -m venv env 
 if [[ ! -d env ]]; then 
     echo -e "\t--Could not create virutal environment...Please make sure venv is installed"
     exit 1
 fi
 
-# 3. Install Requirements 
+# 4. Install Requirements 
 
-echo -e "3. Installing Requirements..."
-if [[ -e "requirements.txt" ]]; then 
+echo -e "4. Installing Requirements..."
+if [[ ! -e "requirements.txt" ]]; then 
     echo -e "\t--Need to requirements.txt to install packages."
     exit 1
 fi
@@ -32,8 +50,8 @@ fi
 source env/bin/activate
 pip install -r requirements.txt
 
-# 4. Install Selenium drivers 
-echo -e "4. Installing Selenium Drivers..."
+# 6. Install Selenium drivers (Notice I'm doing this will the virtual environment is activated!!)
+echo -e "5. Installing Selenium Drivers..."
 if [[ $PLATFORM == 'Linux' ]];  then 
     wget https://github.com/mozilla/geckodriver/releases/download/v0.29.0/geckodriver-v0.29.0-linux64.tar.gz 
     tar -xvf geckodriver-v0.29.0-linux64.tar.gz
@@ -42,7 +60,3 @@ fi
 deactivate 
 echo -e "Install is complete."
 
-#5. Option to run data files or continue to site
-echo e- "Would you like to run the data collection files or continue straight to site?"
-# things in munge
-# things in web_scrape_script
